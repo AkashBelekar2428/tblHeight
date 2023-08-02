02def8c5e48d3c27464fe87b2e406a2b9e829a1a
Akash Belekar <akash.belekar@mobifilia.com>
2023-08-02 17:59:00 +0530

    set name of device

---
+++ tblViewHeight/TVC.swift
@@ -0,0 +1,13 @@
+//
+//  TVC.swift
+//  tblViewHeight
+//
+//  Created by Akash Belekar on 02/08/23.
+//
+
+import UIKit
+
+class TVC: UITableViewCell {
+    @IBOutlet weak var lbl:UILabel!
+
+}
--- tblViewHeight/ViewController.swift
+++ tblViewHeight/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
+struct Laptop {
+    var name:String
+    var price : Int
+    var model : Int
+    
+    init(name: String, price: Int, model: Int) {
+        self.name = name
+        self.price = price
+        self.model = model
+    }
+}
+
 class ViewController: UIViewController {
+    @IBOutlet weak var tbl:UITableView!
+    
+    let Device = Laptop(name: "HP", price: 100, model: 2020)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tbl.register(UINib(nibName: "TVC", bundle: nil), forCellReuseIdentifier: "TVC")
+        tbl.dataSource = self
+        tbl.delegate = self
+        tbl.reloadData()
     }
-
-
 }
 
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Device.price
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TVC", for: indexPath) as! TVC
+        cell.lbl.text = "Laptop Name: \(Device.name)"
+        return cell
+    }
+}
